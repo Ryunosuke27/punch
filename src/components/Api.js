88c4d206af97bd8d5fe138bd2a3f0876a0571f65
林龍之介 <hayashiryuunosuke@hayashiryuunosukenoMacBook-Air.local>
2021-02-08 08:29:37 +0900

    DynamoDBLinkage

--- src/components/Api.js
+++ src/components/Api.js
@@ -3,18 +3,19 @@ import axios from 'axios';
 import Buttons from './ContainedButtons'
 
 const Api = () => {
-    const punchMethod = () =>{
-        axios.post("https://ed1qsxnwla.execute-api.ap-northeast-1.amazonaws.com/prod/punch")
+    const punchMethod = (punchKind) =>{
+        axios.post("https://ed1qsxnwla.execute-api.ap-northeast-1.amazonaws.com/prod/punch",
+        punchKind)
     }
     return (
         <div>
             <Buttons
                 text = "In"
-                punchMethod = {() => punchMethod()}
+                punchMethod = {() => punchMethod("1")}
             />
             <Buttons
                 text = "Out"
-                punchMethod = {() => punchMethod()}
+                punchMethod = {() => punchMethod("2")}
             />
         </div>
     )
