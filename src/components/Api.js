4ec21b85cebac8c1ac7e468114ebd26848d5d3ae
林龍之介 <hayashiryuunosuke@hayashiryuunosukenoMacBook-Air.local>
2021-02-08 09:14:27 +0900

    fix

--- src/components/Api.js
+++ src/components/Api.js
@@ -3,22 +3,22 @@ import axios from 'axios';
 import Buttons from './ContainedButtons'
 
 const Api = () => {
-    const punchMethod = (id) =>{
+    const punchMethod = () =>{
         axios.post("https://ed1qsxnwla.execute-api.ap-northeast-1.amazonaws.com/prod/punch",
             {
                 punchKind: "1"
             }
-        )
+        );
     }
     return (
         <div>
             <Buttons
                 text = "In"
-                punchMethod = {() => punchMethod("1")}
+                punchMethod = {() => punchMethod()}
             />
             <Buttons
                 text = "Out"
-                punchMethod = {() => punchMethod("2")}
+                punchMethod = {() => punchMethod()}
             />
         </div>
     )
