0634b59ec7ed96146d9467bb3474b41624e4d167
林龍之介 <hayashiryuunosuke@hayashiryuunosukenoMacBook-Air.local>
2021-02-09 23:26:29 +0900

    favicon fix

--- src/components/PunchContent.js
+++ src/components/PunchContent.js
@@ -13,7 +13,9 @@ const PunchContent = () => {
             <h1 className="title">Punchの使い方</h1>
             <p　className="contents">勤務開始、終了時にそれぞれのボタンを押すだけで勤務時間を記録してくれます。</p>
             <br/>
-            <p className="contents">またダウンロードボタンを押すと勤務表(CSVファイル)のダウンロードリンクがメールで送られてきます。(48時間を超えると使用出来なくなります。)</p>
+            <p className="contents">またダウンロードボタンを押すと合計稼働時間などをまとめた勤務表(CSVファイル)のダウンロードリンクがメールで送られてきます。</p>
+            <br/>
+            <p className="contents"> ※ ダウンロードリンクは発行から48時間を超えると使用出来なくなります。</p>
         </div>
     )
 }
